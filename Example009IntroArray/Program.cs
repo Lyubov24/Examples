3def29e49ff202eb6dadcf648710b560484d3993
Lyubov Zhudrik <lyubov.zhudrik@gmail.com>
2022-11-16 22:59:10 +0300

    correction

--- Example009IntroArray/Program.cs
+++ Example009IntroArray/Program.cs
@@ -1,4 +1,8 @@
-﻿int Max(int arg1, int arg2, int arg3)
+﻿// Найти максимум из девяти чисел.
+
+
+
+int Max(int arg1, int arg2, int arg3)
 {
     int resalt = arg1;
     if(arg2 > resalt) resalt = arg2;
